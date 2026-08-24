445ba8dbd07ef9e47e0dcd1a4593344e0a079668
Simhadri Venkata Subbareddy <subbareddysimhadri3839w@gmail.com>
2026-08-24 19:02:13 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,13 +1,13 @@
 provider "aws" {
-  region = "ap-south-2"
+  region = "us-east-1"
 }
 
 resource "aws_instance" "one" {
   count                  = 3
-  ami                    = "ami-0199ac7c9fbf9ed83"
+  ami                    = "ami-0b6d9d3d33ba97d99"
   instance_type          = "c7i-flex.large"
-  key_name               = "jai"
-  vpc_security_group_ids = ["sg-003fd9ae7d90334a2 "]
+  key_name               = "project"
+  vpc_security_group_ids = ["sg-045c93bae79aaa0e7"]
   tags = {
     Name = var.instance_names[count.index]
   }
@@ -18,7 +18,7 @@ variable "instance_names" {
 }
 
 resource "aws_s3_bucket" "one" {
-  bucket = "my-project-bucket-123456789"
+  bucket = "project-s3-1234"
   versioning {
     enabled = true
   }
