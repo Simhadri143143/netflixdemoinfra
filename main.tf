4b7ca66fca96a35bf1827513486d49a18d818d92
Simhadri Venkata Subbareddy <subbareddysimhadri3839w@gmail.com>
2026-08-24 19:37:44 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -21,6 +21,10 @@ resource "aws_s3_bucket" "one" {
   bucket = "my-bucket-name"
 }
 
+resource "aws_s3_bucket" "one" {
+  bucket = "jenkins-java-subbu143-2026-bucket" # Must be globally unique
+}
+
 resource "aws_s3_bucket_versioning" "one_versioning" {
   bucket = aws_s3_bucket.one.id
 
